a1dc8612ef7ef29ee13971d27dd155cfb520610e
Devon Walsh <devonwalsh@gmail.com>
2021-03-17 13:49:11 -0400

    commit tree c492d0f5040d13d9dab427f92bdd256cc055a8b5

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(array) { 
+    const output = array.find(input => input.result === 'W')
+    if (output != undefined) {
+      return output.year
+    }
+    else return undefined;
+  }
